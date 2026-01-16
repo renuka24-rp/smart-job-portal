efa46a3550289c59141d36bb3000c61e51dd800e
Renuka patil <renukapatil.1707@gmail.com>
2026-01-17 02:10:14 +0530

    Update JobController.java

--- backend/JobController.java
+++ backend/JobController.java
@@ -1,1 +1,25 @@
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/jobs")
+@CrossOrigin("*")
+public class JobController {
+
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping
+    public Job addJob(@RequestBody Job job) {
+        return jobService.addJob(job);
+    }
+
+    @GetMapping
+    public List<Job> getJobs() {
+        return jobService.getAllJobs();
+    }
+}
 
