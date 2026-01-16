f9e9d6399cf03a8f0257d55db0051f777cb62e44
Renuka patil <renukapatil.1707@gmail.com>
2026-01-17 02:10:59 +0530

    Update JobList.js

--- frontend/JobList.js
+++ frontend/JobList.js
@@ -1,1 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function JobList() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/jobs")
+      .then(res => setJobs(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Available Jobs</h2>
+      {jobs.map(job => (
+        <div key={job.id}>
+          <h4>{job.title}</h4>
+          <p>{job.company} - {job.location}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default JobList;
 
