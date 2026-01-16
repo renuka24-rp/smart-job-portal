34a3b8465b4b6e105bf17b7a172930f9bfbdf635
Renuka patil <renukapatil.1707@gmail.com>
2026-01-17 02:09:59 +0530

    Update ApplicationService.java

--- backend/ApplicationService.java
+++ backend/ApplicationService.java
@@ -1,1 +1,16 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationService {
+
+    private final ApplicationRepository applicationRepository;
+
+    public ApplicationService(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+    public Application apply(Application application) {
+        return applicationRepository.save(application);
+    }
+}
 
