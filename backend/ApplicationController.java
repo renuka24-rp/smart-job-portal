4ca27ab988ba238e2afcbe3ba476f2c5d48f5b3c
Renuka patil <renukapatil.1707@gmail.com>
2026-01-17 02:10:30 +0530

    Update ApplicationController.java

--- backend/ApplicationController.java
+++ backend/ApplicationController.java
@@ -1,1 +1,19 @@
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/apply")
+@CrossOrigin("*")
+public class ApplicationController {
+
+    private final ApplicationService applicationService;
+
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @PostMapping
+    public Application apply(@RequestBody Application application) {
+        return applicationService.apply(application);
+    }
+}
 
